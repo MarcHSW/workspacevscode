e4d08fa3d338ce471a4191fe9efa8220db2dbe61
MarcHSW <marc.d99@web.de>
2022-02-11 13:25:16 +0100

    initial commit

---
+++ bussupervisor/src/main/java/de/hsw/bussupervisor/BussupervisorApplication.java
@@ -0,0 +1,13 @@
+package de.hsw.bussupervisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussupervisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BussupervisorApplication.class, args);
+	}
+
+}
