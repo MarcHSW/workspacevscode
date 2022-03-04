2a4ef8dcb668d9d7f5d0509a02cb15c80372368a
HubertusSagebiel <hubertus-sagebiel@t-online.de>
2022-03-04 11:32:43 +0100

    tabelle eingefügt

--- bussupervisor/frontend/src/App.js
+++ bussupervisor/frontend/src/App.js
@@ -1,28 +1,46 @@
-import React, { useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import BuslinienList from "./BuslinienList";
+import Table from './Table';
+
+let columns = [
+  {
+    heading: 'Name',
+    property: 'name'
+  },
+  {
+    heading: 'Haltestellen',
+    property: 'haltestellen'
+  },
+]
+
+
 
 function App() {
-  const [buslinien, setBuslinien] = useState([{name: '370', haltestellen:['sehnde', 'rethmar']}])
-  const buslinienNameRef = useRef()
+  const [buslinien, setBuslinien] = useState([
+    { name: "370", haltestellen: ["sehnde", "rethmar"] },
+  ]);
+  const buslinienNameRef = useRef();
 
-function handleAddBuslinie(e){
-  const name = buslinienNameRef.current.value
+  function handleAddBuslinie(e) {
+    const name = buslinienNameRef.current.value;
 
-  setBuslinien(prevBuslinien =>{
-    return [...prevBuslinien,{name: name, haltestellen: []}]
-  })
-  buslinienNameRef.current.value = null
-  
-}
+    setBuslinien((prevBuslinien) => {
+      return [...prevBuslinien, { name: name, haltestellen: [] }];
+    });
+    buslinienNameRef.current.value = null;
+  }
 
   return (
     <>
-    <button>Mitarbeitersicht</button>
-    <button>Kundensicht</button>
-    <input ref={buslinienNameRef} type= "text"/>
-    <button onClick={handleAddBuslinie}>Buslinie hinzufügen</button>
-    <BuslinienList buslinien={buslinien} />
-    
+      <button>Mitarbeitersicht</button>
+      <button>Kundensicht</button>
+      <input ref={buslinienNameRef} type="text" />
+      <button onClick={handleAddBuslinie}>Buslinie hinzufügen</button>
+      <Table
+          columns={columns}
+          data={buslinien}
+          propertyAsKey='name' //The data property to be used as a key
+        />
     </>
   );
 }
